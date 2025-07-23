f29d2eb2a57717ff6a65c17b6a8e376ee3144f4a
codebykaifi58 <skaif1694@gmail.com>
2025-07-23 14:22:40 -0700

    Chat-App
    chat-app with login and signup logic

--- Chat-App/front/src/App.jsx
+++ Chat-App/front/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './components/Signup';
+import Chat from './components/chat'; // create this if not exists
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import Signup from './components/Signup';
 
 function App() {
-
   return (
-    <>
-     <Signup/>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
+export default App;
---
+++ Chat-App/front/src/components/chat.jsx
@@ -0,0 +1,5 @@
+
+function Chat() {
+  return <h1>Welcome to Chat Page</h1>;
+}
+export default Chat;
